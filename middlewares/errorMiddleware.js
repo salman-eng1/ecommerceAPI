3419158d9703b68a44a151001e374932ab01cdab
ABO HADI <eng92salman.net@gmail.com>
2023-08-23 02:30:23 +0300

    validation layer

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,12 +1,16 @@
+//catch the error parameters from the apiError and extract data
 const globalError = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
     if (process.env.NODE_ENV == 'development'){
         sendErrorForDev(err, res);
+        console.log(err);
     }else{
         sendErrorForProd(err, res);
 }
 }
+
+//return the response after assigning the extracted params above
 const sendErrorForDev=(err, res) => {
     return res.status(err.statusCode).json({ 
         status: err.status,
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const { param,validationResult } = require('express-validator');
 
-const { getCategories, createCategory, getCategory, updateCategory, deleteCategory } = require("../services/categoryService")
+const {
+  getCategories,
+  createCategory,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../services/categoryService");
 
-router.route('/').get(getCategories).post(createCategory);
-router.route('/:id').get(getCategory).put(updateCategory).delete(deleteCategory);
+router.route("/").get(getCategories).post(createCategory);
+router
+  .route("/:id")
+  //validation process will get the id from the request as calrified in the following rule
+  .get(param('id').isMongoId().withMessage('invalid category id'), 
+  //req will be passed to the validation layer
+  (req, res) => {
+    //validationResult will check according to the rules and return empty value in case of valid id
+    // and will return error info in case of invalid id
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+},getCategory)
+  .put(updateCategory)
+  .delete(deleteCategory);
 module.exports = router;
