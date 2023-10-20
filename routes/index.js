2617a59cb26fe0538864eb15e2b0dc4fa7e353a7
ABO HADI <eng92salman.net@gmail.com>
2023-10-20 18:16:50 +0300

    order service & payment

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const productRoute = require("./productRoutes");
 const userRoute = require("./userRoute");
 const authRoute = require("./authRoute");
 const reviewRoute = require("./reviewRoute");
-// const wishlistRoute = require("./wishlistRoute");
-// const addressRoute = require("./addressRoute");
+const wishlistRoute = require("./wishlistRoute");
+const addressRoute = require("./addressRoute");
 const couponRoute = require("./couponRoute");
 const cartRoute = require("./cartRoute");
-// const orderRoute = require("./orderRoute");
+const orderRoute = require("./orderRoute");
 
 const mountRoutes = (app) => {
   app.use("/api/v1/categories", categoryRoute);
@@ -19,11 +19,11 @@ const mountRoutes = (app) => {
   app.use("/api/v1/users", userRoute);
   app.use("/api/v1/auth", authRoute);
   app.use("/api/v1/reviews", reviewRoute);
-  //   app.use("/api/v1/wishlist", wishlistRoute);
-  //   app.use("/api/v1/addresses", addressRoute);
+    app.use("/api/v1/wishlist", wishlistRoute);
+    app.use("/api/v1/addresses", addressRoute);
     app.use("/api/v1/coupons", couponRoute);
     app.use("/api/v1/cart", cartRoute);
-  //   app.use("/api/v1/orders", orderRoute);
+    app.use("/api/v1/orders", orderRoute);
 };
 
 module.exports = mountRoutes;
